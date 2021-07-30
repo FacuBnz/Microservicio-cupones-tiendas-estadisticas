0b55354d2b2fbbc626c12bb295c782db862e9f80
FacuBnz <facu.j.benitez@gmail.com>
2021-07-30 17:53:34 -0300

    added validation middleware to the 'shops' route

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { getStores } from '../controllers/store.controller'
+import { ValidationGetStores } from '../middlewares/store.middleware'
 
 const router = Router();
 
-router.get('/stores', getStores);
+router.get('/stores', ValidationGetStores, getStores);
 router.post('/stores');
 router.delete('/stores/:id')
 
