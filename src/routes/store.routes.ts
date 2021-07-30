ef2f5af703d68acfede9bb6145a4b424570ae1a3
FacuBnz <facu.j.benitez@gmail.com>
2021-07-30 18:07:48 -0300

    the function to delete a shop has been added to the corresponding route

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { getStores } from '../controllers/store.controller'
+import { getStores, deleteStore } from '../controllers/store.controller'
 import { ValidationGetStores } from '../middlewares/store.middleware'
 
 const router = Router();
 
 router.get('/stores', ValidationGetStores, getStores);
 router.post('/stores');
-router.delete('/stores/:id')
+router.delete('/stores/:id', deleteStore)
 
 export default router
