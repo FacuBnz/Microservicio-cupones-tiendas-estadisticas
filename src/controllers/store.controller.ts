f0d779927f8662993761cc295db290fbc84423c7
FacuBnz <facu.j.benitez@gmail.com>
2021-07-30 17:49:49 -0300

    Created the controller 'Stores' and the function to get 'Stores'

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -3,6 +3,31 @@ import { getRepository } from 'typeorm'
 import { Store } from '../entity/Store'
 
 export const getStores = async (req : Request, res: Response) => {
-    const stores = await getRepository(Store).find({take: 10});
-    return res.json(stores);
+
+    const { page, name } = req.query;
+
+    const count = await getRepository(Store).count();
+
+    if(page){
+        const stores = await getRepository(Store).find({skip: (Number(page)*10-10), take: 10});    
+        return res.json({
+            data: stores,
+            totalStores: count
+        });
+        
+    }
+    
+    if(name){
+        const stores = await getRepository(Store).findOne({name: `${name}`});
+        if(stores){
+
+            return res.json({
+                data: stores,
+                totalStores: count
+            });
+        }else{
+            return res.status(404).json({error: "Not found store"})
+        }
+    }
+
 }
