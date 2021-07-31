2bc56ca0b99813fd534d9ad040722edfb33b7b19
FacuBnz <facu.j.benitez@gmail.com>
2021-07-31 12:33:01 -0300

    the name of the function has been changed and it allows to check if a coupon corresponds to a certain email address

--- src/controllers/coupon.controller.ts
+++ src/controllers/coupon.controller.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import { Coupon } from '../entity/Coupon'
 
-export const getCoupon = async (req: Request, res: Response) => {
-    const coupons = await getRepository(Coupon).find();
-    return res.json(coupons);
+export const checkCoupon = async (req: Request, res: Response) => {
+    const { email, code } = req.query
+    const coupon = await getRepository(Coupon).count({ where: { customerEmail: email, code: code } });
+    if(coupon > 0){
+        return res.json({msg: "code and email validated"});
+    }
+    return res.status(404).json({error: "not found code and email"});
+
 }
