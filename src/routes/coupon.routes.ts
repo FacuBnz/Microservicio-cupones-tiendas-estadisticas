da73f37dfe90537561969db0ff3bc6d92651c542
FacuBnz <facu.j.benitez@gmail.com>
2021-07-31 18:47:37 -0300

    the function to delete a coupon and the function to validate the coupon id have been added to the path

--- src/routes/coupon.routes.ts
+++ src/routes/coupon.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { checkCoupon, createCoupon } from '../controllers/coupon.controller'
-import { validateCoupon } from '../middlewares/coupon.middleware'
+import { checkCoupon, createCoupon, deleteCoupon } from '../controllers/coupon.controller'
+import { validateCoupon, validateIdCoupon } from '../middlewares/coupon.middleware'
 
 const router = Router();
 
 router.get('/coupons', checkCoupon)
 router.post('/coupons', validateCoupon, createCoupon)
 router.patch('/coupons')
-router.delete('/coupons')
+router.delete('/coupons/:id', validateIdCoupon, deleteCoupon)
 
 export default router;
