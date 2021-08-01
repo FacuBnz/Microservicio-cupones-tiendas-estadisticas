02aa417a2a0d9ea7bbc16b9f8db0e8f1d686bc7f
FacuBnz <facu.j.benitez@gmail.com>
2021-08-01 18:27:50 -0300

    middlewares were created to validate id, name and address

--- src/middlewares/store.middleware.ts
+++ src/middlewares/store.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import Joi from 'joi';
 
 export const ValidationGetStores = (req: Request, res: Response, next: NextFunction) => {
 
@@ -6,6 +7,35 @@ export const ValidationGetStores = (req: Request, res: Response, next: NextFunct
     const { page, name } = req.query;
     if(page === "" || name === "" || !isNaN(Number(name))) return res.status(400).json({error: "data incorrect"});
     if(page && Number(page) < 1) return res.status(400).json({error: "data incorrect"});
+
+    next();
+}
+
+export const validateNameAndAddress = async (req: Request, res: Response, next: NextFunction) => {
+
+    const schema = Joi.object({
+        name: Joi.string().required(), 
+        address: Joi.string().required()
+    })
+    try {
+        await schema.validateAsync(req.body)
+    } catch (err) {
+        return res.status(422).json(err);
+    }
+
+    next();
+}
+
+export const validateIdStore = async (req: Request, res: Response, next: NextFunction) => {
+
+    const schema = Joi.object({
+        id: Joi.number().integer().positive().required()
+    })
+    try {
+        await schema.validateAsync(req.params)
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 
     next();
 }
