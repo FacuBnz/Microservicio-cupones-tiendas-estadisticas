dc01d190bc504253463ae9c914847cf0976c81a1
FacuBnz <facu.j.benitez@gmail.com>
2021-08-01 01:13:14 -0300

    the function for assigning coupons and the middleware validating the e-mail have been added to the corresponding path

--- src/routes/coupon.routes.ts
+++ src/routes/coupon.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { checkCoupon, createCoupon, deleteCoupon } from '../controllers/coupon.controller'
-import { validateCoupon, validateIdCoupon } from '../middlewares/coupon.middleware'
+import { checkCoupon, createCoupon, assignCoupon, deleteCoupon } from '../controllers/coupon.controller'
+import { validateCoupon, validateIdCoupon, validateEmail } from '../middlewares/coupon.middleware'
 
 const router = Router();
 
 router.get('/coupons', checkCoupon)
 router.post('/coupons', validateCoupon, createCoupon)
-router.patch('/coupons')
+router.patch('/coupons/:email', validateEmail, assignCoupon)
 router.delete('/coupons/:id', validateIdCoupon, deleteCoupon)
 
 export default router;
